26cdcea55046b9ad128dca7188a34ba88de16048
renbaojie <2013140283@qq.com>
2019-08-28 16:34:21 +0800

    从dev摘樱桃

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -5,6 +5,6 @@ public class A {
         System.out.println(111);
         System.out.println(222);
         System.out.println(333);
-        System.out.println("mster修改的prck");
+        System.out.println("dev修改的picked");
     }
 }
