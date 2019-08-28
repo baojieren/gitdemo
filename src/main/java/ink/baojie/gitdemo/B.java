b6b24af09d435210f4fe61bbefa4cd60d2547358
renbaojie <2013140283@qq.com>
2019-08-28 11:17:49 +0800

    回退到w

--- src/main/java/ink/baojie/gitdemo/B.java
+++ src/main/java/ink/baojie/gitdemo/B.java
@@ -2,8 +2,6 @@ package ink.baojie.gitdemo;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("b-e");
-        System.out.println("b-e");
-        System.out.println("b-e");
+        System.out.println("b-w");
     }
 }
