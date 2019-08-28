bf2834c440b08b4ddbda344cf4ccbe85b14ff473
renbaojie <2013140283@qq.com>
2019-08-28 10:42:14 +0800

    回退到f

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -6,5 +6,7 @@ public class A {
         System.out.println("b");
         System.out.println("c");
         System.out.println("d");
+        System.out.println("e");
+        System.out.println("f");
     }
 }
