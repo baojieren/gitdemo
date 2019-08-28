91fd110cfe4e1076f94ebaa39d684279ccf81602
renbaojie <2013140283@qq.com>
2019-08-28 10:46:43 +0800

    e

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -2,6 +2,6 @@ package ink.baojie.gitdemo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("a-w");
+        System.out.println("a-e");
     }
 }
--- src/main/java/ink/baojie/gitdemo/B.java
+++ src/main/java/ink/baojie/gitdemo/B.java
@@ -2,6 +2,6 @@ package ink.baojie.gitdemo;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("b-w");
+        System.out.println("b-e");
     }
 }
