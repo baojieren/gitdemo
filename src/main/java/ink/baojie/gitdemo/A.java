669321342c7a88d89b4f444e2725bf54cc4d25ba
renbaojie <2013140283@qq.com>
2019-08-28 16:55:30 +0800

    没tag

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -2,12 +2,6 @@ package ink.baojie.gitdemo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("ccc");
-        System.out.println("ccc");
-        System.out.println("ccc");
-        System.out.println("ccc");
-        System.out.println("dev修改的picked");
+        System.out.println("没tag");
     }
 }
