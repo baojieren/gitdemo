8134ec8f265cb4f8193ee0bfedf827f99218bd9a
renbaojie <2013140283@qq.com>
2019-08-28 10:05:23 +0800

    我在dev改了东西

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -4,5 +4,10 @@ public class A {
     public static void main(String[] args) {
         System.out.println("这里是在dev改的");
         System.out.println("第二次编写");
+        System.out.println("我在dev改了东西");
+        System.out.println("我在dev改了东西");
+        System.out.println("我在dev改了东西");
+        System.out.println("我在dev改了东西");
+        System.out.println("我在dev改了东西");
     }
 }
