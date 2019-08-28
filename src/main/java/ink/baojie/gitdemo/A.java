7fdb89da33e93480bd9c5484589809503aea1a5a
renbaojie <2013140283@qq.com>
2019-08-28 10:43:38 +0800

    q

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -2,11 +2,6 @@ package ink.baojie.gitdemo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
-        System.out.println("d");
-        System.out.println("e");
-        System.out.println("f");
+        System.out.println("q");
     }
 }
