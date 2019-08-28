aa5a4eb44b8e8b0dc526346f6457b27b3cf758e7
renbaojie <2013140283@qq.com>
2019-08-28 16:36:36 +0800

    l.add()

--- src/main/java/ink/baojie/gitdemo/B.java
+++ src/main/java/ink/baojie/gitdemo/B.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class B {
     public static void main(String[] args) {
         List l = new ArrayList();
+        l.add(1);
         System.out.println(l.toString());
     }
 }
