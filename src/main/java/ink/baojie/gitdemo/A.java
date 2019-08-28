a2eb5b0b99b36039ceed86a3ce24821937f29358
renbaojie <2013140283@qq.com>
2019-08-28 10:30:47 +0800

    1

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -2,15 +2,6 @@ package ink.baojie.gitdemo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("这里是在dev改的");
-        System.out.println("第二次编写");
-        System.out.println("我在dev改了东西");
-        System.out.println("我在dev改了东西");
-        System.out.println("我在dev改了东西");
-        System.out.println("我在dev改了东西");
-        System.out.println("我在dev改了东西");
-        System.out.println("master改了");
-        System.out.println("master改了");
-        System.out.println("master又改了");
+        System.out.println(1);
     }
 }
