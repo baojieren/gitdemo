5373d2efecde9acbe221c96ab18293dfad1b59ba
renbaojie <2013140283@qq.com>
2019-08-28 10:40:19 +0800

    回推到d

--- src/main/java/ink/baojie/gitdemo/A.java
+++ src/main/java/ink/baojie/gitdemo/A.java
@@ -6,8 +6,5 @@ public class A {
         System.out.println("b");
         System.out.println("c");
         System.out.println("d");
-        System.out.println("e");
-        System.out.println("f");
-        System.out.println("g");
     }
 }
