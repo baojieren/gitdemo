9bbd256cb32fbf1b05d014fff5d93c0f26cec52b
renbaojie <2013140283@qq.com>
2019-09-06 10:14:41 +0800

    一修改
    (cherry picked from commit 03122bc4c021daeb197b8b1eb546ff22202134a4)

--- src/main/java/ink/baojie/gitdemo/GitdemoApplication.java
+++ src/main/java/ink/baojie/gitdemo/GitdemoApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitdemoApplication {
 
     public static void main(String[] args) {
+
+
+
+
         SpringApplication.run(GitdemoApplication.class, args);
     }
 
