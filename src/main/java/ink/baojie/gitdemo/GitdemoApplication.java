f3a1f7b4f88fc03d104f33ee64c822fc980f60c3
renbaojie <2013140283@qq.com>
2019-10-14 17:16:38 +0800

    kkkkkkk

--- src/main/java/ink/baojie/gitdemo/GitdemoApplication.java
+++ src/main/java/ink/baojie/gitdemo/GitdemoApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitdemoApplication {
-
     public static void main(String[] args) {
-
-
-
-
-        SpringApplication.run(GitdemoApplication.class, args);
+        SpringApplication.run( GitdemoApplication.class  ,   args);
     }
 
 }
